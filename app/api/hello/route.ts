ccc43b0ba84677d20ce0264f3264047ff90febd1
Sean Stephenson <sean.m.stephenson@disney.com>
2024-06-12 15:28:07 -0700

    main: no cache

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,8 +1,11 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export async function GET(request: Request) {
   return NextResponse.json({
-    messsage: "Hello World!",
+    messsage: "Hello World!!!",
     random: (Math.floor(Math.random() * (100+ 1))) 
   });
 }
